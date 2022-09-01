e0b2b40606f79aa39bad6f0173b5347b77c2b7ab
ZaferGok <41114328+zzafergok@users.noreply.github.com>
2022-09-01 13:52:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/**
+ * wappalyzer-multiple
+ * Running multiple urls on Wappalyzer
+ *
+ * @author zafergok <zafergok.com>
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+const multipleUrls = require('./utils/urls');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(async () => {
+	init({ clear });
+	input.includes(`help`) && cli.showHelp(0);
+	// input.includes(`urls`) && cli.showHelp(0);
+	multipleUrls();
+
+	debug && log(flags);
+})();
---
+++ utils/urls.js
@@ -0,0 +1,28 @@
+const Wappalyzer = require('wappalyzer');
+const wappalyzer = new Wappalyzer();
+
+const urls = [];
+process.argv.slice(2).forEach(function (val, index, array) {
+	urls.push(val);
+});
+
+async function multipleUrls() {
+	try {
+		await wappalyzer.init();
+
+		const results = await Promise.all(
+			urls.map(async url => ({
+				url,
+				results: await wappalyzer.open(url).analyze()
+			}))
+		);
+
+		console.log(JSON.stringify(results, null, 2));
+	} catch (error) {
+		console.error(error);
+	}
+
+	await wappalyzer.destroy();
+}
+
+module.exports = multipleUrls;
